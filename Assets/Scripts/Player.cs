522a201c5e72809351c8d5202d37d3bd9aac6d14
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2023-10-18 20:10:35 -0300

    Script: Player Moves
    Add Simples Player Movement!

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rig;
+    public Animator anim;
+    public Transform sprite;
+
+    public float speed;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float hMovement = Input.GetAxis("Horizontal");
+        float vMovement = Input.GetAxis("Vertical");
+
+        rig.velocity = new Vector2(hMovement * speed, vMovement * speed);
+    }
+}
