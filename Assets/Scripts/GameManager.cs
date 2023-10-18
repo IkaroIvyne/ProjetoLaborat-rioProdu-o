41e7b141d79ac96f2a86392ab336e170c568b991
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2023-10-18 19:16:55 -0300

    Script: GameManager and Singleton
    Created Game Manager and Singleton.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+
+    }
+
+}
