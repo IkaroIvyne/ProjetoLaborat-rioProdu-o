c48d91c7f28ea6042162982179df7e48921993ea
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2023-11-06 21:36:09 -0300

    Script: Player!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
 
     private Rigidbody2D rig;
     public Animator anim;
-    public Transform sprite;
+    public Transform player;
+
+    public List<Sprite> sprites;
+
+
 
     public float speed;
 
+    public GameObject spritePlayer;
+    public SpriteRenderer _currentSprite;
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        _currentSprite = spritePlayer.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -33,5 +43,29 @@ public class Player : MonoBehaviour
         float vMovement = Input.GetAxis("Vertical");
 
         rig.velocity = new Vector2(hMovement * speed, vMovement * speed);
+
+
+        if (vMovement > 0)
+        {
+            _currentSprite.sprite = sprites[0];
+            Debug.Log(vMovement);
+        }
+         if (vMovement < 0)
+        {
+            _currentSprite.sprite = sprites[1];
+            Debug.Log(vMovement);
+        }
+        if (hMovement < 0)
+        {
+            _currentSprite.sprite = sprites[2];
+            Debug.Log(hMovement);
+
+        }
+         if (hMovement > 0)
+        {
+            _currentSprite.sprite = sprites[3];
+            Debug.Log(hMovement);
+
+        }
     }
 }
